62bb8ee42ac44d6416d66cfd121c93d7764bc23e
Ruairidh Holwill <roore@hotmail.co.uk>
2019-05-06 21:03:42 +0100

    questions 4-6 done

--- functions_js_hw.js
+++ functions_js_hw.js
@@ -60,3 +60,75 @@
 
 // ACTUAL: First Verdict:  The murderer is Mrs. Peacock.
 // Second Verdict:  The murderer is Professor Plum.
+
+
+// Ep 4
+// let suspectOne = 'Miss Scarlet';
+// let suspectTwo = 'Professor Plum';
+// let suspectThree = 'Mrs. Peacock';
+//
+// const declareAllSuspects = function() {
+//   let suspectThree = 'Colonel Mustard';
+//   return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
+// }
+//
+// const suspects = declareAllSuspects();
+// console.log(suspects);
+// console.log(`Suspect three is ${suspectThree}.`);
+
+// EXPECTED: maybe an error with the let variables, not sure if the first console log will like the fact that the let variable has changed only within the function block. If no error:
+// The suspects are Miss Scarlet, Professor Plum, Colonel Mustard.
+// Suspect three is Mrs. Peacock.
+
+// ACTUAL: The suspects are Miss Scarlet, Professor Plum, Colonel Mustard.
+// Suspect three is Mrs. Peacock.
+
+
+// Ep 5
+// const scenario = {
+//   murderer: 'Miss Scarlet',
+//   room: 'Kitchen',
+//   weapon: 'Candle Stick'
+// };
+//
+// const changeWeapon = function(newWeapon) {
+//   scenario.weapon = newWeapon;
+// }
+//
+// const declareWeapon = function() {
+//   return `The weapon is the ${scenario.weapon}.`;
+// }
+//
+// changeWeapon('Revolver');
+// const verdict = declareWeapon();
+// console.log(verdict);
+
+// EXPECTED: No errors so the result should be "The weapon is the Revolver."
+
+// ACTUAL: The weapon is the Revolver.
+
+
+// Ep 6
+let murderer = 'Colonel Mustard';
+
+const changeMurderer = function() {
+  murderer = 'Mr. Green';
+
+  const plotTwist = function() {
+    murderer = 'Mrs. White';
+  }
+
+  plotTwist();
+}
+
+const declareMurderer = function () {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
+
+// EXPECTED: calling the changeMurderer const variable last so the result should be "The murderer is Mr Green"
+
+// ACTUAL: The murderer is Mrs. White.
